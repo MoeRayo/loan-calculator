61f6fd630a053da6dcec52decee4a8ea63a9a65a
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-07-17 05:53:02 +0100

    feat: creation of the global variables to be used in the javascript file

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Gloabl variables
+let amount = document.querySelector("#amount");
+let apr = document.querySelector("#apr");
+let years = document.querySelector("#years");
+let zipcode = document.querySelector("#zipcode");
+let payment = document.querySelector("#payment");
+let total = document.querySelector("#total");
+let totalInterest = document.querySelector("#totalinterest");
