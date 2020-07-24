0803fed5ec151d50408957c230cd39eca21c11fc
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-07-24 03:28:54 +0100

    feat:created the calculate function > Yhis funnction is where all the calculations will be done, based on the users input

--- js/app.js
+++ js/app.js
@@ -6,3 +6,32 @@ let zipcode = document.querySelector("#zipcode");
 let payment = document.querySelector("#payment");
 let total = document.querySelector("#total");
 let totalInterest = document.querySelector("#totalinterest");
+
+let principal = Number(amount.value);
+let interest = Number(apr.value) / 100 / 12;
+let payments = Number(years.value) * 12;
+
+// computation of the monthly payment figure.
+let x = Math.pow(1 + interest, payments);
+let monthly = (principal * x * interest) / (x - 1);
+
+function calculate() {
+	if (isFinite(monthly)) {
+		payment.innerHTML = monthly.toFixed(2);
+		total.innerHTML = (monthly * payments).toFixed(2);
+		totalInterest.innerHTML = (monthly * payments - principal).toFixed(2);
+
+		save(amount.value, apr.value, years.value, zipcode.value);
+
+		try {
+			getLenders(amount.value, apr.value, years.value, zipcode.value);
+		} catch (e) {
+			chart(principal, interest, monthly, payments);
+		}
+	} else {
+		payment.innerHTML = "";
+		total.innerHTML = "";
+		totalInterest.innerHTML = "";
+		chart();
+	}
+}
